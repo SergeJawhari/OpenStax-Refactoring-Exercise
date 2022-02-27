312264d66ae48e86083df23e73a1306b55f9025a
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2022-02-27 03:35:36 -0800

    Adds
    Added comments to every test to describe what is happening

--- test_refactoring_exercise.py
+++ test_refactoring_exercise.py
@@ -1,15 +1,18 @@
-from asyncio.windows_events import NULL
 import io
 import sys
 import unittest
 from refactoring_exercise import Game
 
+# This class consists of all test scenarios for the Game class
 class TestRefactoringExercise(unittest.TestCase):
+
+    # Verify that the correct player was added to the roster.
     def test_correct_player_added(self):
         game = Game()
         game.add('Chet')
         self.assertEqual(game.players[game.current_player],'Chet')
 
+    # Verify that player’s number corresponds to their position and when they were added.
     def test_validate_player_position(self):
         game = Game()
         in_right_position = False
@@ -23,6 +26,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertTrue(in_right_position)
 
+    # Verify that the how many players function will return the number of players in the game.
     def test_how_many_players(self):
         game = Game()
 
@@ -32,6 +36,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game.how_many_players, 3)
 
+    # Verify that the game can be played with 2 or more players. 
     def test_game_is_playable(self):
         game = Game()
 
@@ -40,6 +45,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertTrue(game.is_playable())
 
+    # Verify that the game cannot be played with less than 2 players. 
     def test_game_is_not_playable(self):
         game = Game()
 
@@ -47,6 +53,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertFalse(game.is_playable())
 
+    # Verify that the question will be incremented every time the category is called upon.
     def test_next_question_up(self):
         game = Game()
 
@@ -62,7 +69,8 @@ class TestRefactoringExercise(unittest.TestCase):
         sys.stdout = sys.__stdout__ 
 
         self.assertEqual(printToString.getvalue(), 'Pop Question 1\n')
-        
+
+    # Verify that the position (places) of a new player added is 0.    
     def test_player_position_is_zero(self):
         game = Game()
 
@@ -70,6 +78,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game.places[game.current_player], 0)
 
+    # Verify that the purse of a new player added is 0.
     def test_player_purse_is_zero(self):
         game = Game()
 
@@ -77,6 +86,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game.purses[game.current_player], 0)
 
+    # Verify that a new player does not start inside of the penalty box.
     def test_player_not_in_penalty(self):
         game = Game()
 
@@ -84,6 +94,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertFalse(game.in_penalty_box[game.current_player])
 
+    # Verify that (in penalty box) rolling an odd number will set the penalty box variable to True.
     def test_odd_roll_sets_get_out_variable_to_true(self):
         game = Game()
 
@@ -95,7 +106,8 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertTrue(game.is_getting_out_of_penalty_box)
 
-    def test_even_roll_sets_get_out_variable_to_False(self):
+    # Verify that (in penalty box) rolling an even number will set the penalty box variable to False.
+    def test_even_roll_sets_get_out_variable_to_false(self):
         game = Game()
 
         game.add('Chet')
@@ -106,6 +118,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertFalse(game.is_getting_out_of_penalty_box)
 
+    # Verify that (in penalty box) a roll will be added to the current position of the player (+=)
     def test_player_position(self):
         game = Game()
 
@@ -116,6 +129,9 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game.places[game.current_player], 8)
 
+    # Verify that (in penalty box) rolling more than an 11 will result in the board resetting and 
+    # starting from the beginning with the additional positional increases from the roll. i.e., 
+    # being on 11 and rolling a 4 will result in a new position of 3.
     def test_player_position_looping(self):
         game = Game()
 
@@ -127,6 +143,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game.places[game.current_player], 3)
 
+    # Verify that position 0 corresponds to the Pop category.
     def test_position_zero_pop(self):
         game = Game()
 
@@ -134,6 +151,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game._current_category, 'Pop')
 
+    # Verify that position 4 corresponds to the Pop category.
     def test_position_four_pop(self):
         game = Game()
 
@@ -143,6 +161,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game._current_category, 'Pop')
 
+    # Verify that position 8 corresponds to the Pop category.
     def test_position_eight_pop(self):
         game = Game()
 
@@ -152,7 +171,8 @@ class TestRefactoringExercise(unittest.TestCase):
         game.roll(4)
 
         self.assertEqual(game._current_category, 'Pop')
-
+ 
+    # Verify that position 1 corresponds to the Science category.
     def test_position_one_science(self):
         game = Game()
 
@@ -162,6 +182,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game._current_category, 'Science')
 
+    # Verify that position 5 corresponds to the Science category.
     def test_position_five_science(self):
         game = Game()
 
@@ -172,6 +193,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game._current_category, 'Science')
 
+    # Verify that position 9 corresponds to the Science category.
     def test_position_nine_science(self):
         game = Game()
 
@@ -183,6 +205,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game._current_category, 'Science')
 
+    # Verify that position 2 corresponds to the Sports category.
     def test_position_two_sports(self):
         game = Game()
 
@@ -192,6 +215,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game._current_category, 'Sports')
 
+    # Verify that position 6 corresponds to the Sports category.
     def test_position_six_sports(self):
         game = Game()
 
@@ -202,6 +226,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game._current_category, 'Sports')
 
+    # Verify that position 10 corresponds to the Sports category.
     def test_position_ten_sports(self):
         game = Game()
 
@@ -213,6 +238,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game._current_category, 'Sports')
 
+    # Verify that position 3 corresponds to the Rock category.
     def test_position_three_rock(self):
         game = Game()
 
@@ -222,6 +248,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game._current_category, 'Rock')
 
+    # Verify that position 7 corresponds to the Rock category.
     def test_position_seven_rock(self):
         game = Game()
 
@@ -232,6 +259,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game._current_category, 'Rock')
 
+    # Verify that position 11 corresponds to the Rock category.
     def test_position_eleven_rock(self):
         game = Game()
 
@@ -243,6 +271,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game._current_category, 'Rock')
 
+    # Verify that the right question being asked for the Pop category.
     def test_right_question_asked_pop(self):
         game = Game()
 
@@ -259,6 +288,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(printToString.getvalue(), 'Pop Question 0\n')
 
+    # Verify that the right question being asked for the Science category.
     def test_right_question_asked_science(self):
         game = Game()
 
@@ -275,6 +305,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(printToString.getvalue(), 'Science Question 0\n')
 
+    # Verify that the right question being asked for the Sports category.
     def test_right_question_asked_sports(self):
         game = Game()
 
@@ -291,6 +322,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(printToString.getvalue(), 'Sports Question 0\n')
 
+    # Verify that the right question being asked for the Rock category.
     def test_right_question_asked_rock(self):
         game = Game()
 
@@ -307,7 +339,8 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(printToString.getvalue(), 'Rock Question 0\n')
 
-    def test_question_gets_popped(self):
+    # Verify that once a question is asked, it is popped from the stack.
+    def test_question_gets_popped_from_stack(self):
         game = Game()
 
         game.add('Chet')
@@ -316,6 +349,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(len(game.pop_questions),49)
 
+    # Verify that if the getting out of penalty variable is true, the answer will be correct and the player will be rewarded.
     def test_getting_out_of_penalty_correct_answer(self):
         game = Game()
 
@@ -333,6 +367,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(printToString.getvalue(),'Answer was correct!!!!\nChet now has 1 Gold Coins.\n')
 
+    # Verify that if the getting out of penalty variable is false, nothing will happen, the player will not be rewarded.
     def test_not_getting_out_of_penalty_correct_answer(self):
         game = Game()
 
@@ -350,6 +385,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(printToString.getvalue(), '')
 
+    # Verify that if the getting out of penalty variable is not true, it will move on to the next player.
     def test_move_on_to_next_player(self):
         game = Game()
 
@@ -364,6 +400,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game.current_player, 3)
 
+    # Verify that if the answer is correct, the current players purse will be rewarded with one coin added to their purse.
     def test_correct_answer_reward(self):
         game = Game()
 
@@ -373,7 +410,10 @@ class TestRefactoringExercise(unittest.TestCase):
         game.was_correctly_answered()
 
         self.assertEqual(game.purses[game.current_player], 2)
-
+    
+    # Verify that if the current player’s index is the same value as the length of the list, it will restart the cycle of 
+    # players at the index of 0. 3 players will be indexed as [0,1,2]; therefore, if the current player’s index is 3, it 
+    # does not exist and must restart from 0.
     def test_players_list_order_loops(self):
         game = Game()
 
@@ -389,6 +429,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertEqual(game.current_player, 0)
 
+    # Verify that if a player has 6 coins in their purse, it will result in their victory.
     def test_if_six_coins_wins(self):
         game = Game()
 
@@ -396,6 +437,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertFalse(game._did_player_win())
 
+    # Verify that if a player has less than 6 coins, they do not win.
     def test_if_less_than_six_coins_does_not_win(self):
         game = Game()
 
@@ -403,6 +445,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertTrue(game._did_player_win())
 
+    # Verify that a wrong answer will send a player to the penalty box.
     def test_wrong_answer_penalty(self):
         game = Game()
 
@@ -412,6 +455,7 @@ class TestRefactoringExercise(unittest.TestCase):
 
         self.assertTrue(game.in_penalty_box[game.current_player])
 
+    # Verify that getting an answer wrong will move on to the next player.
     def test_wrong_answer_next_player(self):
         game = Game()
 
