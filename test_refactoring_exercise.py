bed116e6f1a14fd0d49a4dfd1949c6c3bf21a4a5
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2022-02-27 00:00:53 -0800

    Added some tests and created a word document for test scenarios

--- test_refactoring_exercise.py
+++ test_refactoring_exercise.py
@@ -1,3 +1,33 @@
+from audioop import add
 import unittest
-from refactoring_exercise import create_rock_question, is_playable, add, how_many_players, roll, _ask_question, _current_category, was_correctly_answered, wrong_answer, _did_player_win
+from refactoring_exercise import Game
 
+class TestRefactoringExercise(unittest.TestCase):
+    def test_correct_player_added(self):
+        game = Game()
+        game.add('Chet')
+        self.assertEqual(game.players[game.current_player],'Chet')
+
+    def test_validate_player_position(self):
+        game = Game()
+        in_right_position = False
+
+        game.add('Chet')
+        in_right_position = len(game.players) == 1
+        game.add('Bob')
+        in_right_position = len(game.players) == 2
+        game.add('Phil')
+        in_right_position = len(game.players) == 3
+
+        self.assertEqual(in_right_position, True)
+
+    def test_player_matches_roll(self):
+        game = Game()
+
+        game.add('Chet')
+        print(game.roll)
+
+        self.assertEqual(game.players[game.current_player],game.roll(6))
+
+if __name__ == '__main__':
+    unittest.main()
