7722ec81d34e5d673a1aa4e969cce236258472e9
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2022-02-26 20:38:31 -0800

    imported all methods to test

--- test_refactoring_exercise.py
+++ test_refactoring_exercise.py
@@ -1,1 +1,3 @@
 import unittest
+from refactoring_exercise import create_rock_question, is_playable, add, how_many_players, roll, _ask_question, _current_category, was_correctly_answered, wrong_answer, _did_player_win
+
